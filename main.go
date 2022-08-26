6f995b2abecfea6ea07d80b0815b0db5abc00c60
Alex Labanino <ghostrider6610@gmail.com>
2022-08-25 20:59:05 -0400

    Variables and Functions

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, world!")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye, cruel world"
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, someThinElseWasSaid := saySomething()
+
+	fmt.Println("The function returned", whatWasSaid, someThinElseWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "something", "and goodbye"
 }
