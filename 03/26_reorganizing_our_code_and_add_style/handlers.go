06a04077bd67b8454118c9ce294cfac451917602
Alex Labanino <ghostrider6610@gmail.com>
2022-09-01 22:52:49 -0400

    Refactoring code and adding styling

---
+++ 03/26_reorganizing_our_code_and_add_style/handlers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, s string) {
+	panic("unimplemented")
+}
